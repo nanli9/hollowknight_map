86af88e232451e8033ef6ee0f5553cbf02b6d335
nanli9 <nanli9@asu.edu>
2021-01-08 11:17:01 -0700

    hollow knight map
    map for hollow knight

---
+++ finalProject.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector(".gameLogo").addEventListener('mouseover', function() {
+        document.querySelector('.gameLogo').style.height = "700px";
+    });
+    document.querySelector(".hallowNestMap").addEventListener('mouseover', (e) => {
+        //document.querySelector(".hallowNestDescription").style.background = "black";
+    });
+});
